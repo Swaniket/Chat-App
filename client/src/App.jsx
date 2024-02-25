74f60fec93875fc90a084d775dad4d661eeb5719
Swaniket <swaniketchowdhury@gmail.com>
2024-02-25 07:16:14 +0530

    UI | Added basic routes

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,16 @@
+import { Routes, Route, Navigate } from "react-router-dom";
+import { Chat, Login, Register } from "./pages";
 import "./App.css";
 
 function App() {
-  return <>Hello World</>;
+  return (
+    <Routes>
+      <Route path="/" element={<Chat />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="*" element={<Navigate to="/" />} />
+    </Routes>
+  );
 }
 
 export default App;
---
+++ client/src/pages/index.js
@@ -0,0 +1,3 @@
+export { default as Chat } from "./Chat/index";
+export { default as Login } from "./Login/index";
+export { default as Register } from "./Register/index";
